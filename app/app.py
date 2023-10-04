3d33f57aeb5ee33eb4a6185df01dd045b32842c5
Moses <mochengo29@gmail.com>
2023-10-04 14:00:42 +0300

    Created new models and relationships

--- app/app.py
+++ app/app.py
@@ -1,22 +1,78 @@
-from flask import Flask, session, request, make_response, jsonify 
+
+from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, User, Product, Order
+from models import db, User, Product, Review
 
 app = Flask(__name__)
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
 api = Api(app)
 
 
+
+
+class Login(Resource):
+    def get(self):
+        return f'Welcome to the LOGIN route'
+
+api.add_resource(Login,'/login')
+
+class Logout(Resource):
+    def get(self):
+        return f'Welcome to the LOGOUT route'
+
+api.add_resource(Logout,'/logout')
+
+class Sign_Up(Resource):
+    def get(self):
+        return f'Welcome to the SIGN_UP route'
+api.add_resource(Sign_Up,'/signup')
+
+
+class Reviews(Resource):
+    def get(self):
+        reviews = Review.query.all()
+        
+        serialized_reviews = [review.to_dict() for review in reviews]
+        return make_response(jsonify(serialized_reviews), 200)
+    
+    def post(self):
+        data = request.get_json()
+        
+        review_text = data['review_text']
+    
+api.add_resource(Reviews,'/reviews')
+
+class Products(Resource):
+    def get(self):
+        products = Product.query.all()
+        
+        serialized_products = [product.to_dict() for product in products]
+        return make_response(jsonify(serialized_products), 200)
+    
+api.add_resource(Products, '/products')    
+
+class ProductById(Resource):
+    def get(self, id):
+        product = Product.query.get(id)
+        
+        if  product:
+            return jsonify(product.to_dict())
+        else:
+            return {'error': 'Product not found'}
+
+api.add_resource(ProductById,'/products/<int:id>')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5555)
+    app.run(port=5555, debug=True)
--- app/models.py
+++ app/models.py
@@ -1,48 +1,151 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-user_product_association = db.Table('user_product_association',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
-)
-
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
     
-    products = db.relationship('Product', secondary=user_product_association, backref='users')
-
-class Product(db.Model, SerializerMixin):
+    reviews = db.relationship('Review', back_populates='user')
+    appointments = db.relationship('Appointment', back_populates='user')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'appointments': [appointment.to_dict() for appointment in self.appointments],
+            'reviews': [review.to_dict() for review in self.reviews],
+        }
+    
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    review_text = db.Column(db.String)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', name='fk_review_user_id'),
+        nullable=False
+    )
+    
+    user = db.relationship('User', back_populates='reviews')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'review_text': self.review_text,
+            'rating': self.rating,
+            'created_at': self.created_at.isoformat(),
+            'user_id': self.user_id,
+        }
+    
+class Product(db.Model):
     __tablename__ = 'products'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String) 
     description = db.Column(db.String, nullable=False)
-    is_in_stock = db.Column(db.Boolean, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'image': self.image,
+            'description': self.description,
+            'is_in_stock': self.is_in_stock,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
         
-    users = db.relationship('User', secondary=user_product_association, backref='products')
-
-class Order(db.Model, SerializerMixin):
-    __tablename__ = 'orders'
+class HairService(db.Model):
+    __tablename__ = 'hair_services'
     
     id = db.Column(db.Integer, primary_key=True)
-    made_at = db.Column(db.DateTime, server_default=db.func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float) 
+            
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'image': self.image,
+            
+        }
+        
+class NailService(db.Model):
+    __tablename__ = 'nail_services'
     
-    user = db.relationship('User', backref='orders')
-    products = db.relationship('Product', secondary=user_product_association, backref='orders')
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float) 
+            
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'image': self.image,
+            
+        }
+
+class MakeupService(db.Model):
+    __tablename__ = 'makeup_services'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float) 
+            
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'image': self.image,
+            
+        }                
+
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    service = db.Column(db.String)
+    extra_information = db.Column(db.String)
+    
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', name='fk_appointment_user_id'),
+        nullable=False
+    )
+    
+    user = db.relationship('User', back_populates='appointments')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'date': self.date.isoformat(),
+            'time': self.time,
+            'service': self.service,
+            'extra_information': self.extra_information,
+            'user_id': self.user_id,
+        }
